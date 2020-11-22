1716a4bf96c0d5e72b072135797869ba489e6e2f
prasadbobbilla <54373689+prasadbobbilla@users.noreply.github.com>
2020-11-22 13:04:13 -0600

    Code for automatic reload of web page for every minute is added

--- Web Visualization/javascripts/create-graph.js
+++ Web Visualization/javascripts/create-graph.js
@@ -2,7 +2,7 @@
  * Parse the data and create a graph with the data.
  */
 function parseData(createGraph) {
-	Papa.parse("../data/test2.csv", {
+	Papa.parse("../data/Sheet2.csv", {
 		download: true,
 		complete: function(results) {
 			createGraph(results.data);
@@ -10,60 +10,27 @@ function parseData(createGraph) {
 	});
 }
 
-// function createGraph(data) {
-// 	var years = [];
-// 	var silverMinted = ["situados_paid"];
 
-// 	for (var i = 1; i < data.length; i++) {
-// 		years.push(data[i][0]);
-// 		silverMinted.push(data[i][1]);
-// 	}
-
-// 	console.log(years);
-// 	console.log(silverMinted);
-
-// 	var chart = c3.generate({
-// 		bindto: '#chart',
-// 	    data: {
-// 	        columns: [
-// 	        	silverMinted
-// 	        ]
-// 	    },
-// 	    axis: {
-// 	        x: {
-// 	            type: 'category',
-// 	            categories: years,
-// 	            tick: {
-// 	            	multiline: false,
-//                 	culling: {
-//                     	max: 15
-//                 	}
-//             	}
-// 	        }
-// 	    },
-// 	    zoom: {
-//         	enabled: true
-//     	},
-// 	    legend: {
-// 	        position: 'right'
-// 	    }
-// 	});
-// }
 
 function createGraph(data){
-	var instance = ["instance"];
+	var instance = [];
 	var bitcoin = ["Bitcoin"];
-	var Etherium = ["Etherium"]
-	 
+	var Etherium = ["Ethereum"]
+	// var Deque = require("collections/deque");
+	//var deque = new Deque(["Bitcoin-Deque"]);
+	//let deque = new Deque(["Bitcoin-Deque"]);
+
 	for (var i = 1; i < data.length-1; i++) {
-		instance.push(data[i][0]);
-		bitcoin.push(data[i][1]);
-		Etherium.push(data[i][2]);
+		instance.push(data[i][1]);
+		bitcoin.push(data[i][2]);
+		Etherium.push(data[i][3]);
+		//deque.addFront(data[i][2]);
 	} 
 
 	console.log(instance);
 	console.log(bitcoin);
 	console.log(Etherium);
+	//console.log(deque);
 
 	var chart = c3.generate({
 		bindto: '#chart',
@@ -92,6 +59,47 @@ function createGraph(data){
 	        position: 'right'
 	    }
 	});
+
+	function timedRefresh(timeoutPeriod) {
+		setTimeout("location.reload(true);",timeoutPeriod);
+	}
+	
+	window.onload = timedRefresh(60000);
+
 }
+
+// class Deque {
+//     constructor() {
+//         this.front = this.back = undefined;
+//     }
+//     addFront(value) {
+//         if (!this.front) this.front = this.back = { value };
+//         else this.front = this.front.next = { value, prev: this.front };
+//     }
+//     removeFront() {
+//         let value = this.peekFront();
+//         if (this.front === this.back) this.front = this.back = undefined;
+//         else (this.front = this.front.prev).next = undefined;
+//         return value;
+//     }
+//     peekFront() { 
+//         return this.front && this.front.value;
+//     }
+//     addBack(value) {
+//         if (!this.front) this.front = this.back = { value };
+//         else this.back = this.back.prev = { value, next: this.back };
+//     }
+//     removeBack() {
+//         let value = this.peekBack();
+//         if (this.front === this.back) this.front = this.back = undefined;
+//         else (this.back = this.back.next).back = undefined;
+//         return value;
+//     }
+//     peekBack() { 
+//         return this.back && this.back.value;
+//     }
+// }
+
+// demo
 
 parseData(createGraph);
